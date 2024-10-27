07bcdc2993683615a7ae97e0492fc91d48af0dda
dylan <dylantsouy@gmail.com>
2024-10-27 23:27:08 +0800

    Setup for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,22 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  base: '/wedding-card/',
+  plugins: [
+    react(), 
     svgr({
       exportAsReactComponent: true,
-    }),],
+    })
+  ],
+  prettier: {
+    configFile: './.prettierrc.json',
+  },
   css: {
     preprocessorOptions: {
       scss: {
         charset: false,
-        additionalData: `@import "@/variables.scss";` 
+        additionalData: `@import "@/variables.scss";`
       }
     }
   },
